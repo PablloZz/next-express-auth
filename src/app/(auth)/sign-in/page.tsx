e3a30c4ddb998c9d5fb93959d59b9cf03a9ce6ff
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-06-22 14:26:13 +0300

    fix: Update naming

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css";
 
-export default function Page() {
+export default function SignIn() {
   return (
     <form className={styles.form}>
       <h1 className={styles.heading}>Sign in</h1>
