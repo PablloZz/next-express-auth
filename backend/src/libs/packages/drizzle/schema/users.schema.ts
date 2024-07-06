8634eb42224d67bea8c0be49bc58dae2d333c33a
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-07-06 14:58:00 +0300

    feat: Add password hash/salt to user schema

--- backend/src/libs/packages/drizzle/schema/users.schema.ts
+++ backend/src/libs/packages/drizzle/schema/users.schema.ts
@@ -1,11 +1,11 @@
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
-
-// TODO: Add password related fields when implement tokens logic
+import { pgTable, serial, timestamp, varchar, integer } from "drizzle-orm/pg-core";
 
 const users = pgTable("users", {
   id: serial("id").primaryKey(),
   email: varchar("email", { length: 255 }).unique().notNull(),
   username: varchar("username", { length: 255 }).notNull(),
+  passwordHash: varchar("passwordHash", { length: 255 }).notNull(),
+  passwordSalt: integer("passwordSalt").notNull(),
   createdAt: timestamp("createdAt").defaultNow().notNull(),
   updatedAt: timestamp("updatedAt").defaultNow().notNull(),
 });
