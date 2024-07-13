f4d75d91abe284a8b0cf956af302144e58420d9b
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-07-13 12:02:50 +0300

    feat: Implement validateSignInPayload middleware

--- backend/src/packages/auth/libs/middleware/authValidation.middleware.ts
+++ backend/src/packages/auth/libs/middleware/authValidation.middleware.ts
@@ -1,9 +1,14 @@
 import { type NextFunction, type Request, type Response } from "express";
-import { signUpValidationSchema } from "../validation";
+import { signInValidationSchema, signUpValidationSchema } from "../validation";
 
 function validateSignUpPayload(req: Request, _: Response, next: NextFunction) {
   signUpValidationSchema.parse(req.body);
   next();
 }
 
-export { validateSignUpPayload };
+function validateSignInPayload(req: Request, _: Response, next: NextFunction) {
+  signInValidationSchema.parse(req.body);
+  next();
+}
+
+export { validateSignInPayload, validateSignUpPayload };
