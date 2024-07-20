640992e4652dfab530f85da8b5bb041e199006d5
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-07-20 08:56:25 +0300

    feat: Implement UnauthorizedError

--- backend/src/libs/enums/index.ts
+++ backend/src/libs/enums/index.ts
@@ -11,6 +11,7 @@ const HttpCode = {
   SUCCESS: 200,
   CREATED: 201,
   BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
   FORBIDDEN: 403,
   NOT_FOUND: 404,
   UNPROCESSED_ENTITY: 422,
@@ -22,6 +23,8 @@ const ExceptionMessage = {
   USER_NOT_FOUND: "User not found",
   INVALID_CREDENTIALS: "Password or email is incorrect",
   INTERNAL_SERVER_ERROR: "An error occurred while processing your request. Please try again later",
+  AUTHORIZATION_HEADER: "Authorization header should be in format: Bearer <token>",
+  INVALID_TOKEN: "Invalid token",
 };
 
 export { TokenExpirationTime, ApiPath, HttpCode, ExceptionMessage };
--- backend/src/libs/exceptions/index.ts
+++ backend/src/libs/exceptions/index.ts
@@ -3,3 +3,4 @@ export { HttpError } from "./httpError.exception";
 export { InternalServerError } from "./internalServerError.exception";
 export { NotFoundError } from "./notFoundError.exception";
 export { BadRequestError } from "./badRequestError";
+export { UnauthorizedError } from "./unauthorizedError.exception";
