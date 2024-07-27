4bfbd008972859a9b02f22aa93856f6e241f4d54
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-07-27 16:44:45 +0300

    feat: Implement signUp api handler

--- frontend/src/packages/auth/index.ts
+++ frontend/src/packages/auth/index.ts
@@ -1,2 +1,23 @@
-export { type SignUpRequestDto, type SignInRequestDto } from "./libs/types/index";
+import { config } from "@/libs/packages/config";
+import { type AuthResponseDto, type SignUpRequestDto } from "./libs/types";
+import { ApiPath, HttpContentType, HttpHeaderKey, HttpMethod } from "@/libs/enums";
+import { AuthApiPath } from "./libs/enums";
+import { parseResponse } from "@/libs/helpers";
+
+async function signUp(
+  newUser: Omit<SignUpRequestDto, "confirmPassword">,
+): Promise<AuthResponseDto> {
+  const response = await fetch(`${config.env.api.PATH}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`, {
+    method: HttpMethod.POST,
+    headers: {
+      [HttpHeaderKey.CONTENT_TYPE]: HttpContentType.APPLICATION_JSON,
+    },
+    body: JSON.stringify(newUser),
+  });
+
+  return await parseResponse<AuthResponseDto>(response);
+}
+
+export { type SignUpRequestDto, type SignInRequestDto } from "./libs/types";
 export { signUpValidationSchema, signInValidationSchema } from "./libs/validationSchemas";
+export { signUp };
