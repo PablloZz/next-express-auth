608830976acb6965374fb41d343baa48b04844b2
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-07-30 09:06:31 +0300

    feat: Update error response form

--- backend/src/libs/middleware/handleError.middleware.ts
+++ backend/src/libs/middleware/handleError.middleware.ts
@@ -5,14 +5,20 @@ import { ExceptionMessage, HttpCode } from "@/libs/enums";
 
 function handleError(err: unknown, _req: Request, res: Response, _next: NextFunction) {
   if (err instanceof ZodError) {
-    return res.status(HttpCode.UNPROCESSED_ENTITY).send(err.flatten());
+    res.statusMessage = ExceptionMessage.VALIDATION_ERROR;
+    return res.status(HttpCode.UNPROCESSED_ENTITY).send({ message: err.flatten() });
   }
 
   if (err instanceof HttpError) {
-    return res.status(err.status).send(err.message);
+    res.statusMessage = err.message;
+    return res.status(err.status).send({ message: err.message });
   }
 
-  return res.status(HttpCode.INTERNAL_SERVER_ERROR).send(ExceptionMessage.INTERNAL_SERVER_ERROR);
+  res.statusMessage = ExceptionMessage.INTERNAL_SERVER_ERROR;
+
+  return res
+    .status(HttpCode.INTERNAL_SERVER_ERROR)
+    .send({ message: ExceptionMessage.INTERNAL_SERVER_ERROR });
 }
 
 export { handleError };
